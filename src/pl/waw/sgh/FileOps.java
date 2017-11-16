86eb67ce4931999dd298b13e8ac28ee5cbea5816
tw67821 <t.wietrzyk1@gmail.com>
2017-11-16 15:02:28 +0100

    Email

--- src/pl/waw/sgh/FileOps.java
+++ src/pl/waw/sgh/FileOps.java
@@ -1,4 +1,20 @@
 package pl.waw.sgh;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class FileOps {
+   static final String FILE_LOC = "H:\\myfile.txt";
+
+   public static void main(String[] args) throws FileNotFoundException {
+        File file = new File(FILE_LOC);
+        Scanner scanner = new Scanner(file);
+       while (scanner.hasNext()){
+           String myLine= scanner.nextLine();
+           System.out.println(myLine);
+           String[] elem = myLine.split(":");
+           for (String el : elem)
+               System.out.println("= " +el);
+       }
+    }
 }
