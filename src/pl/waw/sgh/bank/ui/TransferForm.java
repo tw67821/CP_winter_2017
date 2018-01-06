edb1d86c6f4befb3dd7e29186afb3af25dcc6ebc
Wiate_000 <t.wietrzyk1@gmail.com>
2018-01-06 15:36:51 +0100

    hom

---
+++ src/pl/waw/sgh/bank/ui/TransferForm.java
@@ -0,0 +1,10 @@
+package pl.waw.sgh.bank.ui;
+
+import javax.swing.*;
+
+public class TransferForm {
+    private JTextField FromAccount;
+    private JTextField ToAccount;
+    private JTextField Amount;
+    private JButton transferMoneyButton;
+}
